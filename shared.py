e338d20730d10b2d226ddcece8b62dd0019f1494
marpappi <pappijn.marie@gmail.com>
2025-05-21 17:11:29 +0200

    further editing

--- shared.py
+++ shared.py
@@ -5,6 +5,6 @@ import pandas as pd
 import openpyxl
 
 app_dir = Path(__file__).parent
-tips = pd.read_csv(app_dir / "tips.csv")
+
 data = pd.read_excel("MordernDataAnalytics.xlsx")
 data['ecSignatureDate'] =pd.to_datetime(data['ecSignatureDate'])
